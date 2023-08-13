bc15317952dbcd9a32c0fe846aaafbdd8e22ac02
Dev Ataide <atddevegest@gmail.com>
2023-08-12 23:02:40 -0300

    Create Account Funcional, teste individual realizado.

--- src/CSU/services/AccountServices/createAccountService.ts
+++ src/CSU/services/AccountServices/createAccountService.ts
@@ -1,50 +1,50 @@
-// import prismaClient from "../../../prisma";
+import prismaClient from "../../../prisma";
 
-// interface AccountRequest{
-//     name:   string;
-//     nameBusiness: string;
-//     userid: number;
-//     email:  string;
-//     numberContact: string;
-//     status: boolean;
-//     password:   string;
-// }
+interface AccountRequest{
+    name:   string;
+    nameBusiness: string;
+    userid: number;
+    email:  string;
+    numberContact: string;
+    status: boolean;
+    password:   string;
+}
 
-// class CreateAccountService{
-//     async execute({name,nameBusiness,userid,email,numberContact,status,password}: AccountRequest){
+class CreateAccountService{
+    async execute({name,nameBusiness,userid,email,numberContact,status,password}: AccountRequest){
 
-//         //Verificação de email presente na interface
-//         if(email == '' || email == " "){
-//             throw new Error ("Email Inexistente no Body da Requisição")
-//         }
+        //Verificação de email presente na interface
+        if(email == '' || email == " "){
+            throw new Error ("Email Inexistente no Body da Requisição")
+        }
 
-//         //Verificação de email já cadastrado
-//         const AccountExists = await prismaClient.account.findFirst({
-//             where:{
-//                 email:email
-//             }
-//         })
+        //Verificação de email já cadastrado
+        const AccountExists = await prismaClient.account.findFirst({
+            where:{
+                email:email
+            }
+        })
 
-//         if (AccountExists){
-//             console.log("Email já cadastrado")
-//             throw new Error ("Email já cadastrado")
-//         }
+        if (AccountExists){
+            console.log("Email já cadastrado")
+            throw new Error ("Email já cadastrado")
+        }
 
-//         const Account = await prismaClient.account.create({
-//             data:{
-//                 nameOwner: name,
-//                 nameBusiness : nameBusiness,
-//                 userId : userid,
-//                 email : email,
-//                 numberContact: numberContact,
-//                 status: status,
-//                 password: password,
-//             },
-//        })
-//         console.log(Account)
-//         return Account;
-//     }
+        const Account = await prismaClient.account.create({
+            data:{
+                nameOwner: name,
+                nameBusiness : nameBusiness,
+                userId : userid,
+                email : email,
+                numberContact: numberContact,
+                status: status,
+                password: password,
+            },
+       })
+        console.log(Account)
+        return Account;
+    }
  
-// }
+}
 
-// export {CreateAccountService}
+export {CreateAccountService}
